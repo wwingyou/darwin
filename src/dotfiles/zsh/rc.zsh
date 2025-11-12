0791acbb064fa92fdeaa4b7c930543aabde1ddfc
Seung Gyu Lee <seunggyulee@linecorp.com>
2025-11-12 13:26:12 +0900

    Auto open tmux only in kitty terminal

--- src/dotfiles/zsh/rc.zsh
+++ src/dotfiles/zsh/rc.zsh
@@ -1,7 +1,9 @@
 # attach to tmux after login to shell
 if command -v tmux &>/dev/null; then
-  if [ -z "$TMUX" ]; then
-    tmux attach -t main || tmux new -s main
+  if [ "$TERM" = "xterm-kitty" ]; then
+    if [ -z "$TMUX" ]; then
+      tmux attach -t main || tmux new -s main
+    fi
   fi
 fi
 
