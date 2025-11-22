45fe25a1f5ca4325862c85d24af6a7f7181c04b5
Seung Gyu Lee <seunggyulee@linecorp.com>
2025-11-22 15:41:21 +0900

    Add warning sign on public git repository

--- src/dotfiles/zsh/rc.zsh
+++ src/dotfiles/zsh/rc.zsh
@@ -11,3 +11,18 @@ fi
 if [ -f "$HOME/.zshrc_custom" ]; then
   source "$HOME/.zshrc_custom"
 fi
+
+# this function runs everytime user change directory
+function chpwd() {
+  if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+
+    if git remote -v | grep -Eq "github\.com.+\(push\)$"; then
+      echo -e "
+      \e[31m!!WARNING!!
+
+      This repository pushes your code to the public.
+      Please be sure no private data is store in this directory.
+      "
+    fi
+  fi
+}
